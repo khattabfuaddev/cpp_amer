5737b4ca9c50ef8e49a13d567b0426e2be548be0
khattabfuaddev <khattabfuad@hotmail.com>
2012-12-23 02:25:24 -0800

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,29 @@
+//the hello question I don't remembre the order of these questions
+#include<iostream.h>
+int main(){
+char a,c;
+cout<<"Enter a word that contains 5 letters";
+cin>>a;
+if(a=='h'){
+c++;
+};
+cin>>a;
+if(a=='e'){
+c++;
+];
+cin>>a;
+if(a=='l'){
+c++;
+};
+cin>>a;
+if(a=='l'){
+c++;
+};
+cin>>a;
+if(a=='o'){
+c++/
+};
+if(c==5){
+cout<<"Hello";
+};
+}
