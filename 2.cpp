531249510368bd3737f1a2967a97da1995c5f98a
khattabfuaddev <khattabfuad@hotmail.com>
2012-12-23 02:13:43 -0800

    create first.cpp
    this is the seconed program 

---
+++ 2.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<math.h>
+int main(){
+float r;
+cout<<"Add a radius";
+cin>>r;
+//the space of the circle 
+cout<<"\n the space of the circle is "<<3.14*pow(r,2);
+//pertimetre
+cout<<"the Perimeter of the the circle is "<<2*r*3.14;
+//ball's size
+cout<<"\n the size of the ball is "<<.75*pow(r,3);
+}
