21597ab7caa1bc599a5e13cc7165ef67fc206823
khattabfuaddev <khattabfuad@hotmail.com>
2012-12-23 02:18:53 -0800

    this is the first question

---
+++ 1.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<math.h>
+int main(){
+//asking the user about the 3 numbers
+cout<<"insert 3 numbers in a row";
+float a,b,c;
+cin>>a>>b>>c;
+cout<<"the sum of these numbers is "<<a+b+c;
+cout<<"\n the multiplcation of these numbers is "<<a*b*c;
+cout<<"\n the 2nd power  of the sum is "<<pow(a+b+c,2);
+cout<<"\n the 2nd power of the multiplaction is  "<<pow(a*b*c);
+//End of this program
+}
