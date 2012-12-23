e2675f5ba8bd1dc64fadfe37744c3b6b80698e83
khattabfuaddev <khattabfuad@hotmail.com>
2012-12-23 10:43:47 -0800

    Create functiion5.cpp
    this this the last quesiton

---
+++ functiion5.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<math.h>
+int main(){
+int x,y;
+cout<<"Enter two numbers in a row";
+if(x>10){
+cout<<pow(x,2)+y*5;
+};
+if(10=>x&&x>0){
+cout<<pow(x,2)+pow(y,2);
+};
+if(x<0){
+cout<<x+5;
+};
+}
